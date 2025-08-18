ff677c56ab410cb58907cfc368d8057d9d7b2b45
Gabriele Leonardi <leonardi.gabriele.lg@gmail.com>
2025-08-18 13:22:30 +0700

    fix(page): add prefetch option to Link component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Page() {
   let photos = Array.from({ length: 6 }, (_, i) => i + 1);
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <section className="cards-container">
       {photos.map((id) => (
-        <Link className="card" key={id} href={`/photos/${id}`} passHref>
+        <Link className="card" key={id} href={`/photos/${id}`} passHref prefetch={false}>
           {id}
         </Link>
       ))}
